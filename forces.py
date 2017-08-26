1c76e4640502596be5857199a4d5b5ace7837370
Daniel Pasut <daniel.j.pasut@gmail.com>
2017-08-26 08:43:48 -0400

    removed broken code

--- forces.py
+++ forces.py
@@ -76,90 +76,9 @@ def find_B(pos, theta, R, N, wr):
                        R*(np.sin(theta[k])-np.sin(theta[k-1])),
                        p/N])
         cross += C * np.cross(dl, r) / LA.norm(r)**3
-
     return cross
 
 
-def find_pos(xforce, yforce, zforce, pos, t):
-    accx = xforce/m
-    accy = yforce/m
-    accz = zforce/m
-
-    disp[0] = V[0]*t + (1/2)*accx*t**2
-    disp[1] = V[1]*t + (1/2)*accy*t**2
-    disp[2] = V[2]*t + (1/2)*accz*t**2
-
-    if LA.norm(disp) < 1e-4:
-        running = False
-    else:
-        running = True
-
-    newposx = pos[0] + disp[0]
-    newposy = pos[1] + disp[1]
-    newposz = pos[2] + disp[2]
-
-    return newposx, newposy, newposz, running
-
-
-def run():
-    count = 0
-    while True:
-        t = count * tstep
-        pos = position[count]
-        Bx, By, Bz = find_B(pos, theta, R, N, wr)
-        Bx_right, By_right, Bz_right = find_B(pos, theta, R, N, wr)
-        Bx_left, By_left, Bz_left = find_B(pos, theta, R, N, wr)
-        Bx_up, By_up, Bz_up = find_B(pos, theta, R, N, wr)
-        Bx_down, By_down, Bz_down = find_B(pos, theta, R, N, wr)
-
-        bxy = (Bx_right - Bx_left) / 2*h
-        byy = (By_right - By_left) / 2*h
-        bzy = (Bz_right - Bz_left) / 2*h
-
-        bxz = (Bx_up - Bx_down) / 2*h
-        byz = (By_up - By_down) / 2*h
-        bzz = (Bz_up - Bz_down) / 2*h
-
-        # X derivatives calculated by divergence and curl of B
-        bxx = -byy - bzz
-        byx =  bxy
-        bzx =  - bxz
-
-        BdotGradB[count, 0] = Bx*bxx + By*bxy + Bz*bxz
-        BdotGradB[count, 1] = Bx*byx + By*byy + Bz*byz
-        BdotGradB[count, 2] = Bx*bzx + By*bzy + Bz*bzz
-
-        if count > 0:
-            V[0] = (position[count, 0] - position[count -1, 0])/tstep
-            V[1] = (position[count, 1] - position[count -1, 1])/tstep
-            V[2] = (position[count, 2] - position[count -1, 2])/tstep
-        else:
-            V[0] = 0
-            V[1] = 0
-            V[2] = 0
-
-        Forcex = MagConst*BdotGradB[count, 0] + ViscConst*V[0]
-        Forcey = MagConst*BdotGradB[count, 1] + ViscConst*V[1]
-        Forcez = -m*g + (4/3)*np.pi*Rc**3*rhow*g + MagConst*BdotGradB[count, 0] + ViscConst*V[2]
-
-        print(Forcex, Forcey, Forcez)
-
-        position[count + 1, 0], position[count + 1, 1], position[count + 1, 2], running= find_pos(Forcex, Forcey, Forcez, pos, t)
-
-        print(count)
-        #print(position[count])
-
-        if count == maxcount:
-            print("count exceded")
-            break
-        elif position[count + 1, 2] <= 2*p*n:
-            print('position too low')
-            break
-        elif running == False:
-            print('not moving')
-            break
-        else:
-            count += 1
 
 if __name__ == '__main__':
     run()
